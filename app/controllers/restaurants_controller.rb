36396edd93a407f0c5b691420a67e83b825a8850
Alam Khalid <alam.khalid@devsinc.com>
2022-04-29 16:21:48 +0500

    Subject: Item filter by category
    * Items filter: Item filter through ajax

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,31 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: %i[update show]
+
   def index
     @restaurants = Restaurant.all
   end
 
+  def update
+    case params[:category_name]
+    when 'all'
+      @items = @restaurant.items
+    when 'popular'
+      # find one with most order count
+    else
+      @items = Item.includes(:categories).where(restaurant_id: params[:id])
+                   .where(categories: { name: params[:category_name] })
+    end
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def show
+  end
+
+  private
+
+  def find_restaurant
     @restaurant = Restaurant.find(params[:id])
-    @categories = Category.order(:name)
   end
 end
