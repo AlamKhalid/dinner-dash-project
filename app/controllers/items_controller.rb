4f81e67dac7ef6e8222f0f51a83ea8643f578d14
Alam Khalid <alam.khalid@devsinc.com>
2022-05-12 14:44:20 +0500

    Subject: Validation and Flash messages
    * Validations: Item, order, user validations
    * Flash: updating flash messages for success and error showing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,9 @@
 
 # Controller for items
 class ItemsController < ApplicationController
-  before_action :find_restaurant, only: %i[show new create destroy edit update]
-  before_action :find_item, only: %i[show edit destroy update]
-  before_action :authorize_admin, only: %i[new edit create update destroy]
+  before_action :find_restaurant, only: %i[show new create edit update]
+  before_action :find_item, only: %i[show edit update]
+  before_action :authorize_admin, only: %i[new edit create update]
 
   def show; end
 
@@ -26,29 +26,31 @@ class ItemsController < ApplicationController
 
   def create
     @item = @restaurant.items.new(item_params)
-    params[:item][:category_ids].each { |id| @item.categories << Category.find(id) if id.length.positive? }
     save_item_check
-  end
-
-  def destroy
-    @item.destroy ? flash[:notice] = 'Item deleted successfully' : flash[:else] = 'An error occured'
-    redirect_to edit_restaurant_path(@restaurant)
   end
 
   private
 
+  def check_item_picture
+    return if @item.item_picture.attached?
+
+    @item.item_picture.attach(io: File.open('./app/assets/images/stock_img.jpeg'),
+                              filename: "stock_img_#{@item.id}.jpeg")
+  end
+
   def save_item_check
     if @item.save
+      check_item_picture
       flash[:notice] = 'Item created successfully'
       redirect_to edit_restaurant_path(@restaurant)
     else
-      flash[:alert] = 'An error occured'
+      flash.now[:notice] = 'An error occured'
       render 'new'
     end
   end
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :retired)
+    params.require(:item).permit(:name, :description, :price, :retired, :item_picture, category_ids: [])
   end
 
   def authorize_admin
