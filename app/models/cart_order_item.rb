e5543ab422b64723a4a1e7ee16875e6f85320de9
Alam Khalid <alam.khalid@devsinc.com>
2022-05-15 14:50:24 +0500

    Subject: Validations on FE

--- app/models/cart_order_item.rb
+++ app/models/cart_order_item.rb
@@ -3,4 +3,7 @@
 class CartOrderItem < ApplicationRecord
   belongs_to :cart_order
   belongs_to :item
+
+  validates :item_id, :quantity, :type, presence: true
+  validates :quantity, numericality: { greater_than: 0 }
 end
