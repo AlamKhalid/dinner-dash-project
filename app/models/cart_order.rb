2f9106b8c13a1012627a82b1298a7d91151254ba
Alam Khalid <alam.khalid@devsinc.com>
2022-05-16 12:39:26 +0500

    Subject: Removed unnecessary comment

--- app/models/cart_order.rb
+++ app/models/cart_order.rb
@@ -7,7 +7,6 @@ class CartOrder < ApplicationRecord
   has_many :items, through: :cart_order_items
 
   validates :user_id, :total_price, :type, :restaurant_id, presence: true
-  # validates :cart_order_items, length: { minimum: 1 }
   validates :total_price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ },
                           numericality: { greater_than: 0, less_than: 1_000_000 }
 end
