45e058406ddfaad5bb76aab5dcbb180d454b2790
Alam Khalid <alam.khalid@devsinc.com>
2022-05-17 01:07:30 +0500

    Subject: Add scopes

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,9 @@ class Order < CartOrder
     cancelled: 2,
     completed: 3
   }, _prefix: true
+
+
+  scope :all_orders, -> { includes(:user, :restaurant).order(:id).all }
+  scope :with_status, ->(status) { includes(:user, :restaurant).order(:id).where(status: status).all }
+  scope :for_user, ->(uid) { includes(:restaurant, :cart_order_items).order(created_at: :desc).where(user_id: uid) }
 end
