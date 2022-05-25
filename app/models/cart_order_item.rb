5f6144c3d0031274ddf6f270e6f0f35806cf3637
Alam Khalid <alam.khalid@devsinc.com>
2022-05-25 21:58:05 +0500

    Subject: Model level validations
    * Cart Order: Validations for belonging to only one user if type is cart
    * Cart Order Item: Validtion for being only 1 for particular item and cart_order_id

--- app/models/cart_order_item.rb
+++ app/models/cart_order_item.rb
@@ -4,6 +4,7 @@ class CartOrderItem < ApplicationRecord
   belongs_to :cart_order
   belongs_to :item
 
+  validates :item_id, uniqueness: { scope: :cart_order_id }
   validates :item_id, :quantity, :type, presence: true
   validates :quantity, numericality: { greater_than: 0 }
 end
