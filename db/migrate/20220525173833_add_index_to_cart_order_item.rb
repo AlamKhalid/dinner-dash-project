b9d93e964a25b8bd4815cb10d54e90285634fa7a
Alam Khalid <alam.khalid@devsinc.com>
2022-05-26 14:24:38 +0500

    Subject: Rubocop offenses removal

--- db/migrate/20220525173833_add_index_to_cart_order_item.rb
+++ db/migrate/20220525173833_add_index_to_cart_order_item.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# unique index for cart order item
 class AddIndexToCartOrderItem < ActiveRecord::Migration[5.2]
   def change
     add_index :cart_order_items, %i[cart_order_id item_id], unique: true
