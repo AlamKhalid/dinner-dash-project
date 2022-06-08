a4fb1b8960992e5c029a1050afff7999451d86c0
Alam Khalid <akhalid.bscs18seecs@seecs.edu.pk>
2022-06-08 14:43:57 +0500

    Subject: Refactored Restaurant Rspec
    * Added description
    * Added context
    * Added unique

---
+++ spec/factories/restaurant.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :restaurant do
+    name { 'Res1' }
+    location { 'Islamabad' }
+  end
+end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,42 +1,63 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  subject = described_class.new(name: 'Res1', location: 'Islamabad')
+  let(:restaurant) { FactoryBot.build :restaurant }
+  let(:restaurant_dup) { FactoryBot.build :restaurant }
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+  # it 'is valid if the name is unique' do
+  #     expect(subject).to be_valid
+  # end
+
+  describe '.creation' do
+    context 'when valid' do
+      it 'can be created' do
+        expect(restaurant).to be_valid
+      end
+    end
+
+    context 'when invalid' do
+      it 'cannot be created without name' do
+        restaurant.name = nil
+        expect(restaurant).not_to be_valid
+      end
+
+      it 'cannot be created without location' do
+        restaurant.location = nil
+        expect(restaurant).not_to be_valid
+      end
+    end
   end
 
-  it 'is not valid without a name' do
-    subject.name = nil
-    expect(subject).not_to be_valid
+  describe '.association' do
+    context 'with valid associations' do
+      it 'has many items' do
+        items = described_class.reflect_on_association(:items)
+        expect(items.macro).to eq(:has_many)
+      end
+
+      it 'has many cart_orders' do
+        cart_orders = described_class.reflect_on_association(:cart_orders)
+        expect(cart_orders.macro).to eq(:has_many)
+      end
+    end
   end
 
-  it 'is not valid without a location' do
-    subject.location = nil
-    expect(subject).not_to be_valid
-  end
+  describe '.uniqueness' do
+    context 'when name is not unique' do
+      it 'is invalid' do
+        restaurant.save
+        expect(restaurant_dup).to be_invalid
+      end
+    end
 
-  it 'has many items' do
-    items = described_class.reflect_on_association(:items)
-    expect(items.macro).to eq(:has_many)
-  end
-
-  it 'has many cart_orders' do
-    cart_orders = described_class.reflect_on_association(:cart_orders)
-    expect(cart_orders.macro).to eq(:has_many)
-  end
-
-  before { described_class.create(name: 'Res1', location: 'Islamabad') }
-
-  it 'is invalid if the name is not unique' do
-    expect(subject).to be_invalid
-  end
-
-  context 'when name is unique' do
-    it 'is valid if the name is unique' do
-      subject = described_class.new(name: 'Res1', location: 'Islamabad')
-      expect(subject).to be_valid
+    context 'when name is unique' do
+      it 'is valid' do
+        restaurant_dup.name = 'Res2'
+        restaurant.save
+        expect(restaurant_dup).to be_valid
+      end
     end
   end
 end
