500bd1e4abe681ae208eeb758a19fe126ab32d45
Alam Khalid <akhalid.bscs18seecs@seecs.edu.pk>
2022-06-10 02:16:55 +0500

    Subject: Restaurant Post Create testing
    * For unauth user
    * For normal user
    * For admin user

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -32,6 +32,11 @@ RSpec.describe RestaurantsController, type: :controller do
         get :new
         expect(response.status).to eq(302)
       end
+
+      it 'redirects to root path' do
+        get :new
+        expect(response).to redirect_to(root_path)
+      end
     end
 
     context 'with non-admin users' do
@@ -40,6 +45,11 @@ RSpec.describe RestaurantsController, type: :controller do
       it 'redirects' do
         get :new
         expect(response.status).to eq(302)
+      end
+
+      it 'redirects to root path' do
+        get :new
+        expect(response).to redirect_to(root_path)
       end
     end
 
@@ -59,6 +69,80 @@ RSpec.describe RestaurantsController, type: :controller do
       it 'creates new restaurant' do
         get :new
         expect(assigns(:restaurant)).to be_a_new(Restaurant)
+      end
+    end
+  end
+
+  describe 'POST create' do
+    context 'with unauthenticated users' do
+      before { allow(controller).to receive(:current_user).and_return(nil) }
+
+      it 'redirects' do
+        post :create
+        expect(response.status).to eq(302)
+      end
+
+      it 'redirects to root path' do
+        post :create
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'with non-admin users' do
+      before { allow(controller).to receive(:current_user).and_return(user) }
+
+      it 'redirects' do
+        post :create
+        expect(response.status).to eq(302)
+      end
+
+      it 'redirects to root path' do
+        post :create
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'with admin users' do
+      let(:valid_params) do
+        {
+          restaurant: {
+            name: 'Ranchers',
+            location: 'Islamabad'
+          }
+        }
+      end
+
+      before { allow(controller).to receive(:current_user).and_return(admin) }
+
+      it 'creates a new restaurant' do
+        expect { post :create, params: valid_params }.to change(Restaurant, :count).by(+1)
+      end
+
+      it 'creates a restaurant with correct attributes' do
+        post :create, params: valid_params
+        expect(Restaurant.last).to have_attributes valid_params[:restaurant]
+      end
+
+      it 'redirects to admin_index_template' do
+        post :create, params: valid_params
+        expect(response).to redirect_to(admins_index_path)
+      end
+
+      it 'sets the flash for success' do
+        post :create, params: valid_params
+        expect(flash[:notice]).to be_present
+      end
+
+      it 'sets error flash for invalid attributes' do
+        valid_params[:restaurant][:name] = nil
+        post :create, params: valid_params
+        expect(flash[:alert]).to be_present
+      end
+
+      it 'renders new template for invalid attributes' do
+        valid_params[:restaurant][:name] = nil
+        post :create, params: valid_params
+        expect(response).to render_template('new')
       end
     end
   end
